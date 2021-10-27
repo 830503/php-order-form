e7eb4a22b718260e2b06e4f770e9c390eb16d050
830503 <lixiaoqi19830503@gmail.com>
2021-10-27 12:01:54 +0200

    added order both food and drinks

--- form-view.php
+++ form-view.php
@@ -20,6 +20,9 @@
             <li class="nav-item">
                 <a class="nav-link" href="?food=0">Order drinks</a>
             </li>
+            <li class="nav-item">
+                <a class="nav-link" href="?food=2">Order both</a>
+            </li>
         </ul>
     </nav>
     <form method="post">
--- index.php
+++ index.php
@@ -20,7 +20,6 @@ function whatIsHappening() {
 //your products with their price.
 
 if(empty($_GET) || $_GET['food'] == '1'){
-
     $products = [
         ['name' => 'Club Ham', 'price' => 3.20],
         ['name' => 'Club Cheese', 'price' => 3],
@@ -28,13 +27,26 @@ if(empty($_GET) || $_GET['food'] == '1'){
         ['name' => 'Club Chicken', 'price' => 4],
         ['name' => 'Club Salmon', 'price' => 5]
     ];
-}else{
+}else if($_GET['food'] == '0'){
     $products = [
         ['name' => 'Cola', 'price' => 2],
         ['name' => 'Fanta', 'price' => 2],
         ['name' => 'Sprite', 'price' => 2],
         ['name' => 'Ice-tea', 'price' => 3],
     ];
+}else{
+    $products = [
+        ['name' => 'Club Ham', 'price' => 3.20],
+        ['name' => 'Club Cheese', 'price' => 3],
+        ['name' => 'Club Cheese & Ham', 'price' => 4],
+        ['name' => 'Club Chicken', 'price' => 4],
+        ['name' => 'Club Salmon', 'price' => 5],
+        ['name' => 'Cola', 'price' => 2],
+        ['name' => 'Fanta', 'price' => 2],
+        ['name' => 'Sprite', 'price' => 2],
+        ['name' => 'Ice-tea', 'price' => 3],
+    ];
+
 }
 
 
