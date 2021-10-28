006d7a65f27f76206c34d7aeac3c7f0afcda3a65
830503 <lixiaoqi19830503@gmail.com>
2021-10-28 10:01:52 +0200

    remove $_SESSEION[]

--- form-view.php
+++ form-view.php
@@ -30,7 +30,7 @@
             
             <div class="form-group col-md-4">
                 <label for="email">E-mail:</label>
-                <input type="text" id="email" name="email" class="form-control" value="<?php echo $_SESSION['email']; ?>"/>
+                <input type="text" id="email" name="email" class="form-control" value="<?php echo $email; ?>"/>
                 <div class="alert alert-primary">
                     <?php echo $errors['email']; ?>
                 </div>
@@ -43,7 +43,7 @@
             <div class="form-row">
                 <div class="form-group col-md-4">
                     <label for="street">Street:</label>
-                    <input type="text" name="street" id="street" class="form-control" value="<?php echo $_SESSION['street']; ?>">
+                    <input type="text" name="street" id="street" class="form-control" value="<?php echo $street; ?>">
                     <div class="alert alert-primary" role="alert">
                         <?php echo $errors['street']; ?>
                     </div>
@@ -51,7 +51,7 @@
                 
                 <div class="form-group col-md-4">
                     <label for="streetnumber">Street number:</label>
-                    <input type="text" id="streetnumber" name="streetnumber" class="form-control" value="<?php echo $_SESSION['streetnumber']; ?>">
+                    <input type="text" id="streetnumber" name="streetnumber" class="form-control" value="<?php echo $streetnumber; ?>">
                     <div class="alert alert-primary" role="alert">
                         <?php echo $errors['streetnumber']; ?>
                     </div>
@@ -61,7 +61,7 @@
             <div class="form-row">
                 <div class="form-group col-md-4">
                     <label for="city">City:</label>
-                    <input type="text" id="city" name="city" class="form-control" value="<?php echo $_SESSION['city']; ?>">
+                    <input type="text" id="city" name="city" class="form-control" value="<?php echo $city; ?>">
                     <div class="alert alert-primary" role="alert">
                         <?php echo $errors['city']; ?>
                     </div>
@@ -69,7 +69,7 @@
                 
                 <div class="form-group col-md-4">
                     <label for="zipcode">Zipcode</label>
-                    <input type="text" id="zipcode" name="zipcode" class="form-control" value="<?php echo $_SESSION['zipcode']; ?>">
+                    <input type="text" id="zipcode" name="zipcode" class="form-control" value="<?php echo $zipcode; ?>">
                     <div class="alert alert-primary" role="alert">
                         <?php echo $errors['zipcode']; ?>
                     </div>
